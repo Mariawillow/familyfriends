2086b8722e51a4698fa5cc23c5aaa3300affd1e2
Mariawillow <mani0013@stud.kea.dk>
2025-04-07 11:33:09 +0200

    opbygning

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Header from"@/components/Header.jsx";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Header />
+    </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { LuBellDot } from "react-icons/lu";
+
+const Header = () => {
+    return ( <div className="flex justify-evenly">
+        <p className="">FamilyFriends</p>
+<LuBellDot />
+
+    </div> );
+}
+ 
+export default Header;
