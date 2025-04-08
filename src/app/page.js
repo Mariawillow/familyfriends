7762bb9bc450472527a2a020e83da83fa119344a
Mariawillow <mani0013@stud.kea.dk>
2025-04-08 10:46:22 +0200

    søger for at alle ikke kan læse mit api

--- src/app/page.js
+++ src/app/page.js
@@ -12,17 +12,15 @@ export default async function Home() {
 
 // optionobjects
   headers: {
-    Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJOTVRITTJCNG5xZTVIcG43QTZtVktReE1uVGw2N3psSlNMRTlmZThjd2NKb3JHWjY2cyIsImp0aSI6ImI2Y2QxOWM5NmU1ODhhMTRkYjMzMjZhMDQ2NmUzMzBhNGMwZWE0MWUzMWIyMzQyNGFiZjBiN2FjODc1ODkxNDk2ZDAwNTdmZTE3MWFmYmRlIiwiaWF0IjoxNzQ0MTAwNDE5LCJuYmYiOjE3NDQxMDA0MTksImV4cCI6MTc0NDEwNDAxOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.KzOtW74RQkZhcGNS0dxwr-4fGbKDF4n50EWALHC3KXuWvTzrTvPd-kzMIBXBZpLQr6nzvlOt2iAJHv0MnyVfnumamqU2iDBsvne7mzlqIjpoLcJakWnT-tUQ0VIdvNC7VwjGBx_cJJY1rO6exOXm9dGTMkQxB3dyZEJQti3-pPZs7kQWG-HJ-ACT1QCYxjlBKVKSTiFcMAOVhkgTo2Ljzdv_m5yobzW-CrMvhnK5X7eRzK8TD6Ek0Wq5PNi0MhvrfmqCTnp4dbYvmuFIRmbQ8o-TFK6wku7NKab41f-4piM-UlXLBezBXeiueg5ZRrBFekwGr4bRKmKU8x_USATQHw"
-
+    Authorization: `Bearer ${process.env.API_TOKEN}`,
   }
   });
   const animals = await data.json();
   console.log(animals);
   return (
     <div>
-      <RemoteImage  />
       <div><Header  /></div>
-      <div> <Footer /></div>
+      <RemoteImage  />
       <div><Slider /></div>
       <div><Listecard /></div>
 
